66d4acb9ee559979910c756dfee645d7094a231e
abdulqadir18 <boxwalaabdulqadir2002@gmail.com>
2021-11-16 12:11:05 +0530

    16nov

---
+++ Java_Practice/demo/finaly.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class finaly {
+  final int a=1;  
+  void change(){  
+  a=2;  
+ }  
+ public static void main(String args[]){  
+ finaly obj=new finaly();  
+ obj.change(); 
+}
+}
